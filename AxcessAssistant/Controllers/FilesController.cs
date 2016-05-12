93f5adba63125f7e142b4f719a07c18968d08238
David Moon <david.moon@wolterskluwer.com>
2016-05-12 18:18:14 -0500

    moved files to webapi and added a controller to download them

---
+++ AxcessAssistant/Controllers/FilesController.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace AxcessAssistant.Controllers
+{
+    public class FilesController : ApiController
+    {
+        public HttpResponseMessage Get(string fileName)
+        {
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+
+            var path = HttpContext.Current.Server.MapPath("~/Files/" + fileName);
+            var stream = new FileStream(path, FileMode.Open);
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentType =
+                new MediaTypeHeaderValue("application/octet-stream");
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = fileName
+            };
+
+            return result;
+        }
+    }
+}
