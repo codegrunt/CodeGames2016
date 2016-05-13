8024a4ba92a514222fef1eb5e343e6e9eb844fc4
jason.pagel <jason.pagel@TX60LP519704>
2016-05-13 00:07:56 -0500

    You can now search for notes.

--- AxcessAssistant/Dialogs/ShowNotesDiag.cs
+++ AxcessAssistant/Dialogs/ShowNotesDiag.cs
@@ -16,12 +16,65 @@ namespace AxcessAssistant.Dialogs
     {
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync("Find notes dialog coming soon");
-            context.Wait(MessageReceivedAsync);
+
+            Client clt = null;
+            context.ConversationData.TryGetValue("client", out clt);
+
+            if (clt != null)
+            {
+                await ShowNotes(context, clt);
+                var bDiag = new BaseDialog();
+                await bDiag.StartOver(context);
+            }
+            else
+            {
+                await context.PostAsync("Which client would you like to pull notes for?");
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        private async Task ShowNotes(IDialogContext context, Client clt)
+        {
+            var cltDal = new ClientDAL();
+            var noteClient = cltDal.GetClientById(clt.ID);
+
+            if (noteClient != null && noteClient.Notes != null && noteClient.Notes.Count > 0)
+            {
+                await context.PostAsync($"Found the following notes for {noteClient.ClientName}:");
+                foreach (var note in noteClient.Notes)
+                {
+                    await context.PostAsync(note.Text);
+                }
+
+            }
+            else
+            {
+                context.ConversationData.RemoveValue("client");
+                await context.PostAsync($"Error finding client notes {clt.ClientName}, resetting to main.");
+            }
         }
 
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<Message> argument)
         {
+            var message = await argument;
+
+            var cltDAL = new ClientDAL();
+            var clts = cltDAL.FindClientsByName(message.Text);
+            if (clts != null && clts.Count > 0)
+            {
+                var clt = clts[0];
+
+                await ShowNotes(context, clt);
+                context.ConversationData.SetValue("client", clt);
+                var bDiag = new BaseDialog();
+                await bDiag.StartOver(context);
+            }
+            else
+            {
+                await context.PostAsync($"Could not find client {message.Text}.  Or client did not have any notes.  Please enter another client.");
+                context.Wait(MessageReceivedAsync);
+            }
+
 
         }
     }
